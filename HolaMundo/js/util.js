4399e6d62c54b2f70ada1f8d0bb31e8c49bd4ade
CursosTardes <miguel17370@gmail.com>
2018-03-02 21:02:12 +0100

    nueva web CM

--- HolaMundo/js/util.js
+++ HolaMundo/js/util.js
@@ -1,2 +1,8 @@
 //Aparecerá en Console de la herramienta del navegador
 console.log("Yo soy un fichero de javascript");
+
+function avisos (){
+    var cadena = "Historia de HTLM5";
+    alert("Hola: " + cadena);
+
+}

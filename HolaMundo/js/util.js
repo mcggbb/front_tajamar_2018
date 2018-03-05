ff951c06b3fe72cccb2a988cc27ff2aca0efd2b1
front_tajamar_2018 <miguel17370@gmail.com>
2018-03-05 12:39:12 +0100

    incorporacion de mensajes en index

--- HolaMundo/js/util.js
+++ HolaMundo/js/util.js
@@ -1,2 +1,7 @@
 //Aparecerá en Console de la herramienta del navegador
 console.log("esto es hola mundo");
+var cadena = "San Miguel, donde va triunfa";
+
+function avisos (){
+    return document.getElementById("mensaje").value = cadena;
+}

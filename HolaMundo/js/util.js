83f8b3bd85d9c65db043d131e1546bd40191942f
front_tajamar_2018 <miguel17370@gmail.com>
2018-03-05 11:55:18 +0100

    nueva documentación

--- HolaMundo/js/util.js
+++
@@ -1,8 +0,0 @@
-//Aparecerá en Console de la herramienta del navegador
-console.log("Yo soy un fichero de javascript");
-
-function avisos (){
-    var cadena = "Historia de HTLM5";
-    alert("Hola: " + cadena);
-
-}

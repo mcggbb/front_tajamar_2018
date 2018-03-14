6a9ac68c27685d6c20026c6a8d4624a6ab1991fb
CursosTardes <miguel17370@gmail.com>
2018-03-14 21:23:31 +0100

    Bloque de Javascript

---
+++ JS/01_funcion/js/calculos.js
@@ -0,0 +1,18 @@
+"use strict"; //Sistema obliga que la semántica a respetar el lenguaje
+
+function sumar(a, b) {//con argumentos
+    var c;
+    c = a + b;
+    console.log("El resultado: ", c);
+    return c;
+}
+
+sumar(1110, 20);
+
+//
+function resta(a, b) {
+    var z = a - b;
+    console.log("El resultado: ", z);
+    return z;
+}
+resta(1110, 20);
---
+++ JS/02_modulo/js/app.js
@@ -0,0 +1,8 @@
+import {saludar} from './app2.js';
+saludar();
+
+/* function otroSaludo(){
+    console.log ("Hola amigo");    
+} */
+
+

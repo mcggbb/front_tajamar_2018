56ddd4dec0fc4b99026d0b910ed73a40dff99e44
CursosTardes <miguel17370@gmail.com>
2018-04-09 21:23:31 +0200

    acordeon, teclado, pruebas

---
+++ JS_Web/05_acordeon/js/acordeon.js
@@ -0,0 +1,52 @@
+"use strict"; //Sistema obliga que la semántica a respetar el lenguaje
+
+/* class Acorden {
+    constructor (panel) {
+        this.oPanel = document.getElementById(panel);
+        this.oElemento = this.oPanel.querySelector("div"); //texto adicional
+        this.oEnlace = this.oPanel.querySelector("p") //enlace	
+        this.oEnlace.addEventListener("click", this.mostrar.bind(this))
+    }
+	
+    mostrar () {
+        this.oElemento.classList.toggle("oculto")
+        if (this.oElemento.className.indexOf("oculto") >=0 ) {
+           this.oEnlace.innerHTML = "Seguir leyendo";
+        }
+        else { // elemento.className incluye "visible")
+            this.oEnlace.innerHTML = "Ocultar artículo";
+        }
+    }
+}
+
+function init() {
+    let n = document.querySelectorAll('.article_long').length
+    let aTextos = []
+    for (let i = 0; i < n; i++) {
+        aTextos.push(new Texto("panel"+(i+1)))
+    }
+}
+
+
+
+    function main() {
+
+
+        var acc = document.getElementsByClassName("accordion");
+        var i;
+
+        for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+                if (panel.style.display === "block") {
+                    panel.style.display = "none";
+                } else {
+                    panel.style.display = "block";
+                }
+            });
+        }
+    }
+    window.addEventListener("load", init)
+
+})(); */
